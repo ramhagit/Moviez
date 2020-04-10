a5cbbb9af4bbe020ad011727e599e445b3ec0860
Hagit Ram <ram1hagit@gmail.com>
2020-04-11 01:24:08 +0300

    style-loader and css-loader dependencies installed to use cssModules for combining of multiple css files

--- movizzz/src/components/Thumbnail.js
+++ movizzz/src/components/Thumbnail.js
@@ -3,23 +3,23 @@ import { useEffect } from 'react';
 import { tmdbImage } from './api';
 
 const Thumbnail = (props) => {
-    const { name, path } = props;
+    const { name, imgSrc, releaseYear, starRate } = props;
 
-    useEffect(async () => {
-        try {
-            const response = await tmdbImage.get(`/w92${path}`);
-            console.log(response);
+    // useEffect(async () => {
+    //     try {
+    //         const response = await tmdbImage.get(`/w92${path}`);
+    //         console.log(response);
             
-            return () => {
-                tmdbImage.CancelToken.source().cancel();
-            }
-        } catch (error) { 
-            console.error(error);
-        }
-    }, [])
+    //         return () => {
+    //             tmdbImage.CancelToken.source().cancel();
+    //         }
+    //     } catch (error) { 
+    //         console.error(error);
+    //     }
+    // }, [])
 
     return(
-        <img alt={name} src={`https://image.tmdb.org/t/p/w92${path}`}/>
+        <img alt={name} src={imgSrc}/>
     )
 }
 
