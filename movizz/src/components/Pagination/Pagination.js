ceb908ed0b74d40a6779747a66ce1795030a233f
Hagit Ram <ram1hagit@gmail.com>
2020-05-06 17:14:18 +0300

    pagination arrows activated

--- movizz/src/components/Pagination/Pagination.js
+++ movizz/src/components/Pagination/Pagination.js
@@ -1,13 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import './Pagination.css';
 
 const Pagination = (props) => {
     const { numOfPages, path } = props;
-    const [activePageIndex, setActivePageIndex] = useState(0);
-    // console.log('path: ', path, 'current url: ', window.location.href);
+
+    const [indexOfFirstToDisplayPage, setIndexOfFirstToDisplayPage] = useState(0);
+    const [pagesDisplaySize, setPagesDisplaySize] = useState(5);
+    const [backwardsDisabled, setBackwardsDisabled] = useState(true);
+    const [forwardsDisabled, setForwardsDisabled] = useState(false);
+
     const currentPage = Number(window.location.href.split('page/')[1]);
+    const prevDisabled = currentPage === 1 ? true : false;
+    const nextDisabled = currentPage === numOfPages ? true : false;
+    // console.log('path: ', path, 'current url: ', window.location.href);
+
+
+    useEffect(() => {
+        if (indexOfFirstToDisplayPage > numOfPages) {
+            setIndexOfFirstToDisplayPage(currentPage - 1);
+        }
+        if (currentPage > indexOfFirstToDisplayPage + pagesDisplaySize || currentPage < indexOfFirstToDisplayPage + 1) {
+            setIndexOfFirstToDisplayPage(currentPage - 1);
+        }
+
+        if (indexOfFirstToDisplayPage >= numOfPages - pagesDisplaySize - 1) {
+            setForwardsDisabled(true);
+        }
+        if (indexOfFirstToDisplayPage + pagesDisplaySize - 1 < numOfPages - 1) {
+            setBackwardsDisabled(true);
+        }
+        if (indexOfFirstToDisplayPage !== 0) {
+            setBackwardsDisabled(false);
+        }
+    }, [currentPage, indexOfFirstToDisplayPage])
+
+    const firstDisabled = indexOfFirstToDisplayPage < pagesDisplaySize ? true : false;
+    const lastDisabled = indexOfFirstToDisplayPage + 1 >= numOfPages - pagesDisplaySize ? true : false;
+    console.log('currentPage: ', currentPage, 'indexOfFirstToDisplayPage: ', indexOfFirstToDisplayPage, 'backwardsDisabled: ', backwardsDisabled, 'forwardsDisabled: ', forwardsDisabled);
 
     const pages = Array.from(new Array(numOfPages), (x, i) => i + 1).map(page => {
         const pageButton = <button
@@ -21,14 +52,38 @@ const Pagination = (props) => {
     });
 
     const paginationNav = <div className="pagination-nav">
-        <button className="pagination-arrow">{'First'}</button>
-        <button className="pagination-arrow">{'<Prev'}</button>
-        <button className="pagination-arrow">{'<'}</button>
-        <div className="pages-bar">{pages}</div>
-        <button className="pagination-arrow">{'>'}</button>
-        <button className="pagination-arrow">{'Next>'}</button>
-        <button className="pagination-arrow">{'Last'}</button>
-    </div>
+        <Link to={`${path}/page/1`}>
+            <button
+                className="pagination-arrow first"
+                onClick={() => { setIndexOfFirstToDisplayPage(0) }}
+                disabled={firstDisabled}
+            >{'First'}</button>
+        </Link>
+        <Link to={`${path}/page/${currentPage - 1}`}>
+            <button
+                className="pagination-arrow prev"
+                disabled={prevDisabled}
+            >{'<Prev'}</button>
+        </Link>
+        <button className="pagination-arrow backward" disabled={backwardsDisabled}>{'<'}</button>
+        <div className="pages-bar">
+            {pages.slice(indexOfFirstToDisplayPage, indexOfFirstToDisplayPage + pagesDisplaySize)}
+        </div>
+        <button className="pagination-arrow forward" disabled={forwardsDisabled}>{'>'}</button>
+        <Link to={`${path}/page/${currentPage + 1}`}>
+            <button
+                className="pagination-arrow next"
+                disabled={nextDisabled}
+            >{'Next>'}</button>
+        </Link>
+        <Link to={`${path}/page/${numOfPages}`}>
+            <button
+                className="pagination-arrow last"
+                onClick={() => { setIndexOfFirstToDisplayPage(numOfPages - pagesDisplaySize) }}
+                disabled={lastDisabled}
+            >{'Last'}</button>
+        </Link>
+    </div >
 
     return (
         <div className="pagination">
