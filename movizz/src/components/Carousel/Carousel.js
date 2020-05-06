f903b038fba06aaf52e1f8f99d68d3defeddc884
Hagit Ram <ram1hagit@gmail.com>
2020-05-06 13:51:44 +0300

    clash between automatic and manual shift in carousel fixed

--- movizz/src/components/Carousel/Carousel.js
+++ movizz/src/components/Carousel/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import LeftArrow from './LeftArrow';
 import RightArrow from './RightArrow';
 import DotButtons from './DotButtons';
@@ -9,34 +9,39 @@ import './Carousel.css';
 
 const Carousel = (props) => {
     const { displayList, activeIndex, setActiveIndex } = props;
+    const [auto, setAuto] = useState(true);
     const length = displayList.length;
     console.log('displayList: ', displayList, 'activeIndex: ', activeIndex, 'displayList at activeIndex: ', displayList[activeIndex]);
 
     useEffect(() => {
-        if (length) {
+        if (auto && length) {
             setTimeout(() => {
-                let index = activeIndex ? activeIndex : 0;
-                let nextIndex = (index + 1) % length;
+                // let index = activeIndex ? activeIndex : 0;
+                // let nextIndex = (activeIndex + 1) % length;
+                let nextIndex = activeIndex === length - 1 ? 0 : activeIndex + 1;
                 console.log('length: ', length, 'nextIndex: ', nextIndex);
 
                 setActiveIndex(nextIndex);
             }, 5000)
         }
-    }, [length, activeIndex])
+    }, [auto, length, activeIndex])
 
     const goToPrevSlide = () => {
+        setAuto(false);
         let index = activeIndex;
         index < 1 ? index = length - 1 : index--;
         setActiveIndex(index);
     }
 
     const goToNextSlide = () => {
+        setAuto(false);
         let index = activeIndex;
         index === length - 1 ? index = 0 : index++;
         setActiveIndex(index);
     }
 
     const goToSlide = (index) => {
+        setAuto(false);
         setActiveIndex(index);
     }
 
