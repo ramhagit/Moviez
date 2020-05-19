0c16a753752a3db45d72b5946e5e4d479b623d8b
Hagit Ram <ram1hagit@gmail.com>
2020-05-19 12:45:58 +0300

    attributes added

--- movizz/src/components/Attributes/Attributes.js
+++ movizz/src/components/Attributes/Attributes.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import tmdb_logo from '../../assets/images/tmdb_logo.svg';
+import tmdb_logo_big from '../../assets/images/tmdb_logo_big.svg';
 
 const Attributes = () => {
     return (
-        <>Thanks to</>
+        <>
+            <h1>Thanks to</h1>
+            <dl>
+                <dt>
+                    <b>Appleseeds Academy</b>, for initiating this project
+                </dt>
+                <dt>
+                    <b>Pinchas Hodadad</b>, from Appleseeds Academy - for mentoring
+                </dt>
+                <dt></dt>
+            </dl>
+            <h2>sources:</h2>
+            <dl>
+                <dt><img
+                    className="tmdb-img"
+                    src={tmdb_logo_big}
+                    alt="tmdb logo" />
+                    &nbsp;https://www.themoviedb.org/
+                </dt>
+            </dl>
+        </>
     )
 }
 
